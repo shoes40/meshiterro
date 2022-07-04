c9252bdd3b08a542e7cd017e4a107e9226901c8b
shoes40 <105543830+shoes40@users.noreply.github.com>
2022-07-04 13:59:54 +0000

    [start]meshiterro

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to:"homes#top"
+  #get 'homes/top'
+  #get 'homes/new'
+  #get 'homes/index'
+  #get 'homes/show'
+  #get 'homes/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
